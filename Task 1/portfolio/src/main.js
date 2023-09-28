5d90c5465432b89e77ab431efaf01d414b5dc990
adi-madur <adityamadur735@gmail.com>
2023-09-28 16:47:24 +0530

    About

--- Task 1/portfolio/src/main.js
+++ Task 1/portfolio/src/main.js
@@ -2,4 +2,5 @@ import "../styles/modern-normalize.css";
 import '../styles/style.css';
 import "../styles/components/header.css"; // --> For Header Section
 import "../styles/components/hero.css" // --> For Hero Section
+import "../styles/components/about.css" // --> For About Section
 import "../styles/utils.css";
